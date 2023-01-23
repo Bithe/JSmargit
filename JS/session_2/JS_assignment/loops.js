2b6757d93ef5cc0334f59f11ecde61e761463ecc
Bithe <farzana.hossain5@gmail.com>
2023-01-23 14:04:59 +0200

    loops

---
+++ JS/session_2/JS_assignment/loops.js
@@ -0,0 +1,21 @@
+//ex 1
+console.log("Ex 2:")
+
+for ( i = 1; i < 100; i++) {
+  if (i % 2 != 0) {
+    console.log(i);
+  }
+}
+
+
+//ex 2
+
+
+console.log("Ex 2:");
+let num=" ";
+for(i=1; i<100; i++){
+    if(i%2 == 0){ 
+        num= num + " "+i;
+        console.log(num);
+    }
+}
