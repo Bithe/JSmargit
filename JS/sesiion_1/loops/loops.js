2d401cc42556415d9acff08da0eec216a7afdfb0
Bithe <farzana.hossain5@gmail.com>
2023-01-23 13:12:10 +0200

    loops

---
+++ JS/sesiion_1/loops/loops.js
@@ -0,0 +1,18 @@
+for(let number=0; number<=12; number+=2){
+
+}
+
+let yourName;
+do{
+    yourName = prompt("");
+}while(!yourName);
+console.log(yourName);
+
+
+
+let number=0;
+
+while(number<=12){
+    console.log(number);
+    number=number+2;
+}
--- JS/sesiion_1/random_num/main.js
+++ JS/sesiion_1/random_num/main.js
@@ -1,21 +1,21 @@
-const randomNum = () =>{
+const randomNum = () => {
 
     let firstNum = Number(prompt("please enter first number"));
     let secondNum = Number(prompt("please enter second number"));
     let thirdNum = Number(prompt("please enter third number"));
-   let sum, multiplication;
+    let sum, multiplication;
 
-if((firstNum || secondNum || thirdNum) > 0 ){
-    sum = firstNum+secondNum+thirdNum;
-    console.log(`${firstNum}, ${secondNum}, ${thirdNum}, 'Total is:'`, sum);
+    if ((firstNum || secondNum || thirdNum) > 0) {
+        sum = firstNum + secondNum + thirdNum;
+        console.log(`${firstNum}, ${secondNum}, ${thirdNum}, 'Total is:'`, sum);
 
-    if((firstNum && secondNum && thirdNum) >= 0){
-        multiplication = firstNum*secondNum*thirdNum;
-        console.log('Total is:', sum, 'Multiplication is:', multiplication);   
-        }   
+        if ((firstNum && secondNum && thirdNum) >= 0) {
+            multiplication = firstNum * secondNum * thirdNum;
+            console.log('Total is:', sum, 'Multiplication is:', multiplication);
+        }
     }
 
-    else if((firstNum && secondNum && thirdNum) <=0){
+    else if ((firstNum && secondNum && thirdNum) <= 0) {
         console.log("only negatives");
     }
 }
