4044a992b34a129eec15779d867aebba40a08f1b
Bithe <farzana.hossain5@gmail.com>
2023-01-23 11:01:31 +0200

    odd even cal upd

--- JS/sesiion_1/odd_even/main.js
+++ JS/sesiion_1/odd_even/main.js
@@ -10,17 +10,33 @@
     // }
 
 
+// function oddEven(){
+//     let number= prompt("please enter a number");
+
+//     if(number%2 == 0){
+//         console.log("the number is even");
+//     }
+
+//     else if(number%2 == 1){
+//         console.log("the number is odd");
+//     }
+  
+// }
+
+// oddEven();
+
 
 function oddEven(){
     let number= prompt("please enter a number");
 
     if(number%2 == 0){
-        console.log("the number is even");
+        console.log(`${number} the number is even`);
     }
 
     else if(number%2 == 1){
-        console.log("the number is odd");
+        console.log(`${number} the number is odd`);
     }
+  
 }
 
 oddEven();
