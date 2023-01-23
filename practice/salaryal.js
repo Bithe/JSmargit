3ff4a220284e4a540c5ec200766ff5891fa01430
Bithe <farzana.hossain5@gmail.com>
2023-01-23 10:17:57 +0200

    salryCal upd

--- practice/salaryal.js
+++ practice/salaryal.js
@@ -1,16 +1,24 @@
-let salary, cal;
+let salary, calExtra, extraHour;
 let hourlySal = prompt('your hourly salary?');
 let hours = prompt('how many hours you worked?');
 
 if(hours<=7){
-    cal = hours*hourlySal;
-    console.log(cal);
+    salary = hours*hourlySal;
+    console.log(salary);
+}
+
+else if(hours<=9){
+    salary = (7*hourlySal)+((hours-7)*hourlySal)*1.5;
+    console.log(salary);
 }
 
 
 else if(hours>7){
     extraHour = hours-7;
-    calExtra= (extraHour*hourlySal)/2;
-    cal= (hours-extraHour)*hourlySal+calExtra;
-    console.log(cal);
+    calExtra= (extraHour*hourlySal)*1.5;
+    salary= (hours-extraHour)*hourlySal+calExtra;
+    console.log(salary);
 }
+
+
+
