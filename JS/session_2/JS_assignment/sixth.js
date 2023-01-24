20bb47dd3ec11befb5ae5b05ded2142a8b474ee5
Bithe <farzana.hossain5@gmail.com>
2023-01-24 13:09:50 +0200

    6th up

--- JS/session_2/JS_assignment/sixth.js
+++ JS/session_2/JS_assignment/sixth.js
@@ -1,15 +1,33 @@
-let sum=0, count=0, avg;
+// let sum=0, count=0, avg;
 
-for(let counter=0; counter<3; counter++){
-    let input=Number(prompt('enter a number'));
+// for(let counter=0; counter<4; counter++){
+//     let input=Number(prompt('enter a number'));
 
+//     if(input == 0){
+//         console.log('you cant give 0');
+//         break;
+//     }
+//     sum+=input;
+//     count++;
+// }
+
+// avg=sum/count;
+// console.log(avg);
+
+//
+
+let sum=0, count=0, input;
+while(input!=0){
+    input=Number(prompt('enter a number'));
     if(input == 0){
-        console.log('you cant give 0');
-        break;
-    }
+              console.log('you cant give 0');
+                 break;
+         }
     sum+=input;
     count++;
 }
-
-avg=sum/count;
+let avg = sum/count;
 console.log(avg);
+
+
+
