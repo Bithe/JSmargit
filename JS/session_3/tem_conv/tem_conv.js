d29fc4b43c1c3bb6a142841d8929a6cd69aea609
Bithe <farzana.hossain5@gmail.com>
2023-01-30 13:56:31 +0200

    tem cal

---
+++ JS/session_3/tem_conv/tem_conv.js
@@ -0,0 +1,7 @@
+const calTemConv = () => {
+    const celsius = document.getElementById('celsius');
+
+    const fahrenheit = document.getElementById('fahrenheit');
+
+    const kelvin = document.getElementById('kelvin');
+};
