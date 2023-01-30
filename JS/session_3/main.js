30e6d8d1b4b77e36d0663a4a9911ffdc902454a1
Bithe <farzana.hossain5@gmail.com>
2023-01-30 13:11:51 +0200

    amout

--- JS/session_3/main.js
+++ JS/session_3/main.js
@@ -4,6 +4,21 @@ const calGasoline = () => {
 
     // const money = +document.querySelector("#money").value; // the + indicate number method in new js
 
+    const answer = document.querySelector('#answer');
+
+    let text;
+const amount = money/price;
+    if(amount>=10){
+        text = `you could get about ${amount} ltr, good now you can go`;
+    }
+
+    else{
+        text = `you could get about ${amount} ltr, sorry you have to stay :()`;
+    }
+
+
+    answer.textContent = text;
+
     console.log(price, money);
 };
 
