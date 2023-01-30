748fc28e0aa35980410f5eb2e8e0dc45750e773c
Bithe <farzana.hossain5@gmail.com>
2023-01-30 14:46:32 +0200

    formula

--- JS/session_3/tem_conv/tem_conv.js
+++ JS/session_3/tem_conv/tem_conv.js
@@ -1,7 +1,34 @@
 const calTemConv = () => {
-    const celsius = document.getElementById('celsius');
 
-    const fahrenheit = document.getElementById('fahrenheit');
+        // get inputs
+    const celsius = Number( document.querySelector('#celsius').value);
 
-    const kelvin = document.getElementById('kelvin');
+    const fahrenheit = Number( document.getElementById('fahrenheit').value);
+
+    const kelvin = document.getElementById('kelvin').value;
+
+     // C to F & K
+
+    const celsiusToFahrenheit = (celsius*1.8)+32;
+
+    const celsiusTokelvin = celsius + 273.15;
+
+    // F to C & K
+
+    const fahrenheitToCelsius = (fahrenheit-32)/1.8;
+
+    const fahrenheitToKelvin = (fahrenheit-32)*1.8+273.15;
+
+    // K to C & F
+
+    const kelvinToCelsius = kelvin-273.15;
+
+    const kelvinToFahrenheit =  (kelvin-273.15)*(9/5)+32;
+
+
+    // show the data in input fields
+
+    //const celsiusVal = document.getElementById("celsius").value;
+    
+    document.getElementById('celsius').innerHTML = (fahrenheit-32)/1.8;
 };
